2b4c08b9b9cfb790432c5f1cf9bbc1b0a7bdc600
Artem <artem.pascihnyk@hotmail.com>
2023-10-09 20:07:20 +0000

    custom error layouts

---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+	<div class="flex flex-col bg-gray-100 w-full h-full min-h-screen  p-12 items-center">
+		<slot />
+	</div>
+</template>
+<script lang="ts" setup></script>
+
+<style scoped></style>
