36e45a4b719264861fc35e1acd0365436dfbe299
Artem <artem.pascihnyk@hotmail.com>
2023-10-12 20:31:07 +0000

    adding server routes

--- composables/useCourse.ts
+++ composables/useCourse.ts
@@ -1,31 +1,9 @@
+import { Lesson, Chapter, Course, LessonWithPath } from "~/types/course";
 import courseData from "./courseData";
 
-type Lesson = {
-  title: string;
-  slug: string;
-  path: string;
-  number: number;
-  downloadUrl: string;
-  sourceUrl?: string;
-  text: string;
-  videoId: number;
-}
-
-type Chapter = {
-  title: string;
-  slug: string;
-  number: number;
-  lessons: Lesson[];
-}
-
-type Course = {
-  title: string;
-  chapters: Chapter[];
-}
-
 export const useCourse = (): Course => {
-  const chapters: Chapter[] = courseData.chapters.map(chapter => {
-    const lessons: Lesson[] = chapter.lessons.map(lesson => ({
+  const chapters: Chapter[] = courseData.chapters.map((chapter: Chapter) => {
+    const lessons: LessonWithPath[] = chapter.lessons.map((lesson: Lesson) => ({
       ...lesson,
       path: `/course/chapter/${chapter.slug}/lesson/${lesson.slug}`
     }));
